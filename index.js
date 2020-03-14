815a8165c0ef516894036d84413afa15641aecda
Hanna Lee <mani21na@gmail.com>
2020-03-14 00:35:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Your code here
 const mapToNegativize = sourceArray => sourceArray.map( s => s =  -(s))
 
-const mapToNoChange = sourceArray => sourceArray.map( s => s)
+const mapToNoChange = sourceArray => sourceArray.map( s => s = s)
