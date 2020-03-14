970075299b54b355521a332d7c1ce16f5d7fdc39
Hanna Lee <mani21na@gmail.com>
2020-03-14 00:40:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const mapToNegativize = sourceArray => sourceArray.map( s => s =  -(s) )
 const mapToNoChange = sourceArray => sourceArray.map( s => s = s )
 
 const mapToDouble = sourceArray => sourceArray.map( s => s = s*2 )
+
+const mapToSquare = sourceArray => sourceArray.map( s => s = s**)
